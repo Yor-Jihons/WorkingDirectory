317179999725a204680e4bcea65efb9c109e9c1a
Yor-Jihons <ryo.malon814@gmail.com>
2025-08-10 18:23:23 +0900

    Tested.

---
+++ main.xTests/SaveProcessorTest.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using WorkingDirectory.CommandLines;
+using WorkingDirectory.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using WorkingDirectory.Processors;
+
+namespace main.xTests;
+
+public class SaveProcessorTest
+{
+    [Fact]
+    public void Test1()
+    {
+        string? curDirPath = System.IO.Path.GetDirectoryName(Environment.CurrentDirectory);
+        string? curDirParentPath = System.IO.Path.GetDirectoryName(System.IO.Path.GetDirectoryName(Environment.CurrentDirectory));
+
+        var processor1 = new SaveProcessor(".");
+        List<string> list1 = ["C:\\sample", "C:\\test"];
+        processor1.Run(list1);
+        Assert.Equal(curDirPath, list1[^1]);
+
+        var processor2 = new SaveProcessor("..");
+        List<string> list2 = ["C:\\sample"];
+        processor2.Run(list2);
+        Assert.Equal(curDirParentPath, list2[^1]);
+
+        var processor3 = new SaveProcessor("C:\\checker");
+        List<string> list3 = ["C:\\sample"];
+        processor3.Run(list3);
+        Assert.Equal("C:\\checker", list3[^1]);
+    }
+}
