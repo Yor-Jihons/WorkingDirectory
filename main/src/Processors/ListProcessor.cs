afca9ab4c26ed1343c2ccaed213d7078cb4b5c55
Yor-Jihons <ryo.malon814@gmail.com>
2025-08-10 20:52:32 +0900

    Added the command LIST.

--- main/src/Processors/ListProcessor.cs
+++ main/src/Processors/ListProcessor.cs
@@ -1,14 +1,23 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WorkingDirectory.Processors
 {
     public class ListProcessor : IProcessor
     {
+        private readonly string[] _list = ["HEAD", "HEAD^", "HEAD^^"];
         public void Run(List<string> history)
         {
-            // TODO: Implement here.
+            int loopCount = Math.Min(_list.Length, history.Count);
+
+            for (int i = 0; i < loopCount; i++)
+            {
+                string tag = _list[i];
+                string path = history[history.Count - 1 - i];
+                Console.WriteLine($"\"{tag}\": {path}");
+            }
         }
     }
 }
