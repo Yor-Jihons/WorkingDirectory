98d0c7f7c89fa589a89c51035ffa80d9b386f1f5
Yor-Jihons <ryo.malon814@gmail.com>
2025-08-10 18:33:30 +0900

    Added the argument.

--- main/src/CommandLines/CmdLine.cs
+++ main/src/CommandLines/CmdLine.cs
@@ -57,6 +57,7 @@ namespace WorkingDirectory.CommandLines
             var builder = new StringBuilder();
             builder.AppendLine("[COMMANDS]");
             builder.AppendLine("WorkingDirectory save [<DIRECTORY_PATH>]");
+            builder.AppendLine("WorkingDirectory save [<DIRECTORY_PATH>]");
             builder.AppendLine("WorkingDirectory load [\"HEAD\" | \"HEAD^\" | \"HEAD^^\"]");
             builder.AppendLine("WorkingDirectory --help");
             builder.AppendLine("WorkingDirectory --version");
