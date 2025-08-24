5f85cf15d0bc175e8c0d9b4b752d70d1b97dee6d
Yor-Jihons <ryo.malon814@gmail.com>
2025-08-24 17:01:52 +0900

    Added the command.

--- main/src/CommandLines/CmdLine.cs
+++ main/src/CommandLines/CmdLine.cs
@@ -62,6 +62,7 @@ namespace WorkingDirectory.CommandLines
             var builder = new StringBuilder();
             builder.AppendLine("[COMMANDS]");
             builder.AppendLine("WorkingDirectory list");
+            builder.AppendLine("WorkingDirectory clear");
             builder.AppendLine("WorkingDirectory save [<DIRECTORY_PATH>]");
             builder.AppendLine("WorkingDirectory load [\"HEAD\" | \"HEAD^\" | \"HEAD^^\"]");
             builder.AppendLine("WorkingDirectory --help");
