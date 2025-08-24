25702050219b5bc5338f5cfa03d2e61288c3d56a
Yor-Jihons <ryo.malon814@gmail.com>
2025-08-24 17:12:56 +0900

    Modified.

--- main/src/Processors/ListProcessor.cs
+++ main/src/Processors/ListProcessor.cs
@@ -10,6 +10,12 @@ namespace WorkingDirectory.Processors
         private readonly string[] _list = ["HEAD  ", "HEAD^ ", "HEAD^^"];
         public void Run(List<string> history)
         {
+            if (history.Count == 0)
+            {
+                Console.WriteLine("The WorkingDirectory has no paths.");
+                return;
+            }
+
             int loopCount = Math.Min(_list.Length, history.Count);
 
             for (int i = 0; i < loopCount; i++)
